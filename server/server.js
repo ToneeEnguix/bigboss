4bb71124b03992d50b8ed3cba399ec3072457409
Hyslach653 <tonies395@gmail.com>
2021-01-15 20:56:32 +0100

    2nd commit

--- server/server.js
+++ server/server.js
@@ -8,22 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 require("dotenv").config();
 
-/* mongoose.set("useCreateIndex", true);
-async function connecting() {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1/bigBoss", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    console.log("Connected to the DB");
-  } catch (error) {
-    console.log(error);
-    console.log(
-      "ERROR: Seems like your DB is not running, please start it up !!!"
-    );
-  }
-}  */
-
  async function connecting(){
 try {
     await mongoose.connect('mongodb+srv://Christopher29:Bauervapor8@cluster0.nruby.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority',
