dad497074e3f7ffdcf7693ffd3406c9b0d019ebb
Hyslach653 <tonies395@gmail.com>
2021-01-16 18:58:42 +0100

    www

--- src/api/API_URL.js
+++ src/api/API_URL.js
@@ -1,4 +1,4 @@
 export const API_URL =
   window.location.hostname === `localhost`
     ? `http://localhost:4000`
-    : `https://bigbosscompetitions.com`;
+    : `https://www.bigbosscompetitions.com`;
